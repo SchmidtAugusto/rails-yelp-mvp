c3b52c954eaa6210fd76e96d2622209ebfa5413a
Augusto Schmidt <augusto_schmidt@outlook.com.br>
2023-05-24 14:40:04 -0300

    Solved

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates_associated :reviews
+  validates :name, :address, :phone_number, presence: true
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian]}
+end
